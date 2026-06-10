6525120b81a28bd0deb49f69f55bd505d2a618eb
NguyenDinhTienAnh <tienanhokok123@gmail.com>
2026-06-10 17:01:17 +0700

    Hoàn thành form
    Hoàn thành form

---
+++ EXAM2026_Ca2/data.js
@@ -0,0 +1,32 @@
+export const mockDATA = [
+    {
+        name:"Trường THPT Nguyễn Huệ",
+        unit:"Phòng GD&DT TP. Thủy Lợi",
+        status:"Hoàn thành",
+        description:"Hoàn thành chuyển đổi số toàn diện: hệ thống quản lý học tập LMS, phần mềm quản lý hành chính, chữ ký số, và hạ tầng mạng Wifi campus.",
+    },
+    {
+        name:"Trường THCS Lê Quý Đôn",
+        unit:"Phòng GD&DT H. Thủy Nguyên",
+        status:"Đang triển khai",
+        description:"Đang triển khai phần mềm quản lý học bá điện tử và hệ thống camera giám sát lớp học. Đã hoàn thành 60%. ",
+    },
+    {
+        name:"Trường THPT Trần Phú",
+        unit:"Phòng GD&DT Q. Hồng Bằng",
+        status:"Chưa bắt đầu",
+        description:"Chưa bắt đầu chuyển đổi số. Đang chờ kế hoạch và nguồn kinh phí từ Sở Giáo dục.",
+    },
+    {
+        name:"Trường THCS Nguyễn Tri Phương",
+        unit:"Phòng GD&DT TP. Thủy Lợi",
+        status:"Đang triển khai",
+        description:"Đang triển khai phần mềm tự chấm điểm và quản lý tài khoản điện tử. Hoàn thành 70%.",
+    },
+    {
+        name:"Trường THPT Nguyễn Huệ",
+        unit:"Phòng GD&DT TP. Hà Nội",
+        status:"Chưa bắt đầu",
+        description:"Chưa bắt đầu chuyển đổi số. Đang chờ kế hoạch và nguồn kinh phí từ Sở Giáo dục.",
+    }
+]
